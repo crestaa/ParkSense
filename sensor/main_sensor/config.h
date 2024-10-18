198a88207b9d3c0c397394e4dfa788d38e3bd999
Gabriele Crestanello <gabricre01@gmail.com>
2024-10-18 14:30:57 +0200

    feat: deepsleep + read data from sensors

--- sensor/main_sensor/config.h
+++ sensor/main_sensor/config.h
@@ -2,3 +2,13 @@
 #define WIFI_PASS "password123"
 #define SERVER_IP "192.168.4.1"
 #define SERVER_PORT 8080
+
+#define TIME_TO_SLEEP  20          // Time device will go to sleep (in seconds)
+
+// DHT config (leave as it is if not in use)
+#define DHTPIN 23   
+#define DHTTYPE DHT22  
+
+// HC-SR04 config (leave as it is if not in use)
+#define TRIGGER_PIN  13 
+#define ECHO_PIN     12 
--- sensor/main_sensor/main_sensor.ino
+++ sensor/main_sensor/main_sensor.ino
@@ -1,23 +1,31 @@
 #include <WiFi.h>
 #include "config.h"
-//#include <DHT.h>
-
-//#define DHTPIN 4     // Digital pin connected to the DHT sensor
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#include <DHT.h>
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASS;
-const char* serverIP = SERVER_IP;  // IP of the Low Level Gateway
+const char* serverIP = SERVER_IP; 
 const int serverPort = SERVER_PORT;
 
-
-
-//DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
 WiFiClient client;
+
+String macAddress;
+
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+
+RTC_DATA_ATTR int bootCount = 0;
 
 void setup() {
   Serial.begin(115200);
-  //dht.begin();
+  dht.begin();
+
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  // HC-SR04 setup
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
   // WiFi connection setup
   randomSeed(micros());
@@ -36,36 +44,88 @@ void setup() {
     Serial.println("Connecting to WiFi "+ String(ssid) + " with password "+ String(password) + " RSSI: "+String(WiFi.RSSI()));
   }
   Serial.println("Connected to WiFi");
+
+  // Get the last 6 characters of MAC address
+  macAddress = WiFi.macAddress();
+  macAddress.replace(":", "");  // Remove colons
+  macAddress = macAddress.substring(macAddress.length() - 6);  // Get last 6 characters
+
+  delay(1000);
+  performTask();
+
+  // Prepare for deep sleep
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Going to sleep for " + String(TIME_TO_SLEEP) + " seconds");
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
 
-void loop() {
-  // TODO: implement sensor reads, remove mockups 
-  float h = 60.7;//dht.readHumidity();      
-  float t = 32.2;//dht.readTemperature();
+float readDistance() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+  
+  if (distance > 400 || distance <= 2) {  // HC-SR04 typical range is 2cm to 400cm
+    return -1;  // Invalid reading
+  }
+  return distance;
+}
 
-  if (isnan(h) || isnan(t)) {
+void performTask() {
+  float h = dht.readHumidity();      
+  float t = dht.readTemperature();
+  float distance = readDistance();
+
+  String data = "";
+  bool hasValidData = false;
+
+  if (!isnan(h) && !isnan(t)) {
+    data += "t:" + String(t) + ",h:" + String(h);
+    hasValidData = true;
+  } else {
     Serial.println("Failed to read from DHT sensor!");
-    return;
   }
 
-  if (client.connect(serverIP, serverPort)) {
-    // TODO: improve interface for sending data
+  if (distance != -1) {
+    if (hasValidData) data += ",";
+    data += "d:" + String(distance);
+    hasValidData = true;
+  } else {
+    Serial.println("Failed to read from HC-SR04 sensor!");
+  }
+
+  // Generate random nonce
+  long nonce = random(1000, 100001);  // Random number between 1000 and 100000
+
+  // Append MAC address and nonce to data
+  if (hasValidData) {
+    data += ",m:" + macAddress + ",n:" + String(nonce);
+  }
+
+  if (hasValidData && client.connect(serverIP, serverPort)) {
     delay(500);
-    String data = "temp:" + String(t) + ",hum:" + String(h);
     client.println(data);
     client.stop();
     Serial.println("Data sent: " + data);
   } else {
-    if(WiFi.status() != WL_CONNECTED){
+    if (WiFi.status() != WL_CONNECTED) {
       while (WiFi.status() != WL_CONNECTED) {
         delay(2000);
         Serial.println("Connecting to WiFi "+ String(ssid) + " with password "+ String(password) + " RSSI: "+String(WiFi.RSSI()));
       }
-    }else{
-        Serial.println("Couldn't connect to server");
+    } else if (!hasValidData) {
+      Serial.println("No valid data to send");
+    } else {
+      Serial.println("Couldn't connect to server");
     }
-    
   }
+}
 
-  delay(30000);  // Wait for 30 seconds before next reading
+void loop() {
+  // This function is not used when using deep sleep
 }
