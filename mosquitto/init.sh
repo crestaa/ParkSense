f8e3e7d8b8f0f79b6df71e8bf347c5ee70a4ad52
Gabriele Crestanello <58698974+crestaa@users.noreply.github.com>
2025-02-14 00:13:28 +0100

    chores

--- mosquitto/init.sh
+++ mosquitto/init.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 
-# Create password file
 touch /mosquitto/config/password_file
 mosquitto_passwd -b /mosquitto/config/password_file "${MQTT_USERNAME}" "${MQTT_PASSWORD}"
 
-# Start Mosquitto
 exec /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
